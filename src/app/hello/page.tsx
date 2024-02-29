83d5955a66c3591fbcf888d2a9442f5add00325d
yashchhatrala25 <yashchhatrala25@gmail.com>
2024-03-01 00:15:46 +0530

    create nextjs project structure and basic routing

---
+++ src/app/hello/page.tsx
@@ -0,0 +1,5 @@
+export default function HelloPage() {
+    return (
+        <h1 className="text-3xl text-center mt-8">Hello Page!!</h1>
+    )
+}
---
+++ src/app/hello/world/page.tsx
@@ -0,0 +1,5 @@
+export default function WorldPage() {
+    return (
+        <h1 className="text-3xl text-center mt-8">World Page!!</h1>
+    )
+}
